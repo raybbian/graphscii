c6f702c3ab1c316816b2b0150a9f6afdcada79ae
Raymond Bian <raybbian@gmail.com>
2023-12-06 14:16:12 -0500

    dfs spanning tree

--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,66 @@
 #include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/depth_first_search.hpp>
 #include <iostream>
 
-using namespace boost;
-using namespace std;
-
-struct first_name_t {
-    typedef vertex_property_tag kind;
+struct name_t {
+    typedef boost::vertex_property_tag kind;
 };
 
-template <class EdgeIter, class Graph>
-void who_owes_who(EdgeIter first, EdgeIter last, const Graph& G) {
-    // Access the propety acessor type for this graph
-    constexpr first_name_t first_name{};
-    typedef typename property_map<Graph,
-            first_name_t>::const_type NameMap;
-    NameMap name = get(first_name, G);
+struct age_t {
+    typedef boost::vertex_property_tag kind;
+};
 
-    typedef typename boost::property_traits<NameMap>
-    ::value_type NameType;
+typedef boost::property<name_t, std::string> WithName;
+typedef boost::property<age_t, int, WithName> WithNameAndAge;
+typedef boost::adjacency_list<boost::hash_setS, boost::vecS, boost::undirectedS, WithNameAndAge, boost::no_property, boost::hash_setS> MyGraphType;
 
-    NameType src_name, targ_name;
+struct tree_edge_visitor : public boost::default_dfs_visitor {
+    std::vector<boost::graph_traits<MyGraphType>::edge_descriptor> &tree_edges;
 
-    while (first != last) {
-        src_name = boost::get(name, source(*first, G));
-        targ_name = boost::get(name, target(*first, G));
-        cout << src_name << " owes "
-             << targ_name << " some money" << endl;
-        ++first;
+    tree_edge_visitor(std::vector<boost::graph_traits<MyGraphType>::edge_descriptor> &edges)
+            : tree_edges(edges) {}
+
+    void tree_edge(boost::graph_traits<MyGraphType>::edge_descriptor e, const MyGraphType& g) {
+        tree_edges.push_back(e);
     }
-}
+};
+
 
 int main() {
-    typedef property<first_name_t, std::string> FirstNameProperty;
-    typedef adjacency_list<vecS, vecS, directedS,
-            FirstNameProperty> MyGraphType;
 
-    typedef pair<int,int> Pair;
-    Pair edge_array[11] = { Pair(0,1), Pair(0,2), Pair(0,3),
-                            Pair(0,4), Pair(2,0), Pair(3,0),
-                            Pair(2,4), Pair(3,1), Pair(3,4),
-                            Pair(4,0), Pair(4,1) };
+    typedef std::pair<int, int> Pair;
+    Pair edge_array[11] = {Pair(0, 1), Pair(0, 2), Pair(0, 3),
+                           Pair(0, 4), Pair(1, 2), Pair(2, 3)};
 
     MyGraphType G(5);
-    for (int i = 0; i < 11; ++i)
-        add_edge(edge_array[i].first, edge_array[i].second, G);
+    for (auto & i : edge_array)
+        add_edge(i.first, i.second, G);
 
-    property_map<MyGraphType, first_name_t>::type name = get(first_name_t(), G);
+    boost::property_map<MyGraphType, name_t>::type name = boost::get(name_t(), G);
+    boost::property_map<MyGraphType, age_t>::type age = boost::get(age_t(), G);
+    boost::put(name, 0, "John");
+    boost::put(age, 0, 19);
+    boost::put(name, 1, "Katarina");
+    boost::put(age, 0, 35);
+    boost::put(name, 2, "Lux");
+    boost::put(age, 0, 19);
+    boost::put(name, 3, "Jarvan");
+    boost::put(age, 0, 24);
+    boost::put(name, 4, "Ahri");
+    boost::put(age, 0, 99);
 
-    boost::put(name, 0, "Jeremy");
-    boost::put(name, 1, "Rich");
-    boost::put(name, 2, "Andrew");
-    boost::put(name, 3, "Jeff");
-    name[4] = "Kinis"; // you can use operator[] too
+    auto iter_range = boost::adjacent_vertices(0, G);
+    while(iter_range.first != iter_range.second) {
+        std::string vertex_name = boost::get(name, *iter_range.first);
+        std::cout << vertex_name << std::endl;
+        iter_range.first++;
+    }
 
-    who_owes_who(edges(G).first, edges(G).second, G);
+    std::vector<boost::graph_traits<MyGraphType>::edge_descriptor> spanning_tree_edges;
+    tree_edge_visitor vis(spanning_tree_edges);
+    boost::depth_first_search(G, boost::visitor(vis));
+
+    for (auto e : spanning_tree_edges) {
+        std::cout << "Edge: " << source(e, G) << " - " << target(e, G) << std::endl;
+    }
 }
