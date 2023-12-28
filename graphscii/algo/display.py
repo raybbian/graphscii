31e64367ae6a5e6d4ce46ae7d023c3fc9b50803f
Raymond Bian <raybbian@gmail.com>
2023-12-28 00:25:06 -0500

    fixed align for even labels

--- graphscii/algo/display.py
+++ graphscii/algo/display.py
@@ -112,7 +112,7 @@ class Display:
                 label_len = len(str(vertex[1]))
                 mid_ind = label_len // 2
                 for c in range(label_len):
-                    self.output[y][x + c - mid_ind] = str(vertex[1])[c]
+                    self.output[y][x + c - mid_ind + 1] = str(vertex[1])[c]
 
     def draw_edges(self):
         for edge in self.ori_edges:
